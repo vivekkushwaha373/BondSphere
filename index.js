39735562f3ca710c0e1d5c27cbabd399db3fbb5c
MyGit <vivekkushwaha734@gmail.com>
2025-01-22 13:28:33 +0530

    port reconfiguration

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 require('dotenv').config();
 
-const PORT = 5000 || process.env.PORT; 
+const PORT = process.env.PORT||4000; 
 
 server.listen(PORT, () => {
     console.log('App started at port no ', PORT);
